6233b7c36e3dde002d8c496de26a64ef165147a8
Kartikey Jaiswal <karan74jaiswal@gmail.com>
2023-09-02 16:48:55 +0530

    Initial Commit with homescreen only

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -2,7 +2,9 @@ import React from "react";
 import { Text, StyleSheet } from "react-native";
 
 const HomeScreen = () => {
-  return <Text style={styles.text}>HomeScreen</Text>;
+  return (
+    <Text style={styles.text}>Hello World, This is my first demo app😀!</Text>
+  );
 };
 
 const styles = StyleSheet.create({
